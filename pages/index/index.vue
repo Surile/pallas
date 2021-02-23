2934418201fb37fe6b38b9546d2c70f12860f46b
夜雨 <mhecy98@gmail.com>
2021-02-23 19:07:14 +0800

    commit init

---
+++ pages/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view class="container">
+		
+	</view>
+</template>
+
+<script lang="ts">
+	export default {
+		data() {
+			return {
+				title: 'Hello',
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style lang="scss">
+	
+</style>
