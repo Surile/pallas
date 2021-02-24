75449d5390ea4df0f6dc8b66f6b31410556d4d30
夜雨 <mhecy98@gmail.com>
2021-02-24 22:20:33 +0800

    home

---
+++ pages/detail/index.vue
@@ -0,0 +1,30 @@
+<template>
+	<BaseLayout>
+		<view class="container">
+			<view class="ty-row ty-row__align--start">
+				<view class="upload-container ty-row ty-row__direction--column ty-row__align--center">
+					
+				</view>
+				<view class="tips-container">
+					<view class="tips-header"> 提示</view>
+					<view class="tips-content">
+						<view class="tips-item">网站所有文件内容均由用户自行上传分享，本站严格遵守国家相关法律法规，尊重著作权、版权等第三方权利，如果当前文件侵犯了您的相关权利，请邮件反馈至net909@163.com，我们将及时处理。</view>
+					</view>
+				</view>
+			</view>
+		</view>
+	</BaseLayout>
+</template>
+
+<script>
+import { BaseLayout } from '@/components'	
+
+export default {
+	components:{
+		BaseLayout
+	}
+}
+</script>
+
+<style>
+</style>
---
+++ pages/list/index.vue
@@ -0,0 +1,39 @@
+<template>
+	<BaseLayout>
+		<view class="container">
+			<view class="ty-row">
+				<view class="upload-container"></view>
+				<view class="tips-container"></view>
+			</view>
+		</view>
+	</BaseLayout>
+</template>
+
+<script lang="ts">
+import { BaseLayout } from '@/components'
+	
+export default {
+	components:{
+		BaseLayout
+	},
+	data(){
+		return {
+			
+		}
+	},
+	methods:{
+		
+	},
+}
+</script>
+
+<style lang="scss">
+.container{
+	background-color: #FFFFFF;
+	.upload-container{
+		padding: 20px;
+		margin-bottom: 20px;
+		border-radius: 2px;
+	}
+}
+</style>
