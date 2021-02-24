ccf967a4533120b1aef05e29446aa1cccc2cc14f
夜雨 <mhecy98@gmail.com>
2021-02-24 19:02:57 +0800

    commit scss

---
+++ components/BaseLayout/BaseLayout.vue
@@ -0,0 +1,33 @@
+<template>
+	<view class="container">
+		<Header></Header>
+		<slot></slot>
+		<Footer></Footer>
+	</view>
+</template>
+
+<script>
+import { Header,Footer } from '@/components'
+
+export default {
+	name:"BaseLayout",
+	components:{
+		Header,
+		Footer
+	},
+	data() {
+		return {
+			
+		};
+	}
+}
+</script>
+
+<style lang="scss">
+@import '@/styles/index.scss';
+.container{
+	@include display-flex;
+	@include justify-content()
+	margin: 0px auto;
+}
+</style>
---
+++ components/index.ts
@@ -0,0 +1,3 @@
+export { default as Footer } from './Footer/Footer.vue'
+export { default as Header } from './Header/Header.vue'
+export { default as BaseLayout } from './BaseLayout/BaseLayout.vue'
