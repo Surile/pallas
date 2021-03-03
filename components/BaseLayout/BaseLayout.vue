1114fac7f8c0e693f63d62e3b7297db4fa38b78d
夜雨 <mhecy98@gmail.com>
2021-03-03 10:28:53 +0800

    自适应

--- components/BaseLayout/BaseLayout.vue
+++ components/BaseLayout/BaseLayout.vue
@@ -1,10 +1,9 @@
 <template>
 	<view class="container ty-row__direction--column ty-row__justify--between">
-		<Header></Header>
 		<view class="content">
 			<slot></slot>
 		</view>
-		<Footer></Footer>
+		<!-- <Footer></Footer> -->
 	</view>
 </template>
 
@@ -28,10 +27,11 @@ export default {
 <style lang="scss">
 @import '@/styles/index.scss';
 .container{
-	margin: 0px auto;
+	// margin: 0px auto;
+	margin-top: 20px;
 }
 .content{
-	padding: 0px 135px;
+	// padding: 0px 135px;
 	margin-bottom: 20px;
 }
 </style>
