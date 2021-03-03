3576a0052c032adcaae9fc772889cb4c4e197402
夜雨 <mhecy98@gmail.com>
2021-03-03 18:30:25 +0800

    ts 支持

--- sfc.d.ts
+++ sfc.d.ts
@@ -1,4 +1,10 @@
-declare module '*.vue' {
-  import Vue from 'vue'
-  export default Vue
+import Vue from "vue";
+
+declare module "uview-ui";
+
+declare module "*.vue" {
+  import Vue from "vue";
+  export default Vue;
 }
+
+// 全局变量设置
