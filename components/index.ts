5202db20abc6d9b86bb91c4cec26903ed1591383
夜雨 <mhecy98@gmail.com>
2021-03-04 21:21:53 +0800

    vue-cli

--- components/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Footer } from './Footer/Footer.vue'
-export { default as Header } from './Header/Header.vue'
-export { default as BaseLayout } from './BaseLayout/BaseLayout.vue'
-export { default as Card } from './Card/Card.vue'
-export { default as Input } from './Input/Input.vue'
